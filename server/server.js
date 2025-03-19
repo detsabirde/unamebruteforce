fbf6ab11023a7abf527578d5a7821b12739843d2
detsabirde <130311972+detsabirde@users.noreply.github.com>
2025-03-19 17:20:04 +0100

    initialize server and add login page

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname, '../pages/login.html'));
+});
+
+app.post('/',(req, res)=>{
+    
 });
 
 app.listen(port, () => {
